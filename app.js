36217fc74114c63d31a2c5575da57564fe08591b
danishsshaikh <danishsshaikh@gmail.com>
2020-11-04 01:50:52 +0530

    setting up mongoose

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const date = require(__dirname + "/date.js");
 
 const app = express();
@@ -10,6 +9,8 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser : true});
 
 app.get("/", function(req, res) {
 
